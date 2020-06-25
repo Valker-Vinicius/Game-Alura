f98b3ad3fd2973db8b01e236e4f1a6e12052d787
Valker-Vinicius <valkerm2v@gmail.com>
2020-06-25 15:15:00 -0300

    Preloads Separated

---
+++ scripts/preloads.js
@@ -0,0 +1,10 @@
+function preload() {
+    backgroundImage = loadImage('images/background/floresta.png')
+    gameOverImage = loadImage('images/assets/game-over.png')
+    characterImage = loadImage('images/character/correndo.png')
+    enemyImage = loadImage('images/enemies/gotinha.png')
+    trollImage = loadImage('images/enemies/troll.png')
+    wingSlimeImage = loadImage('images/enemies/gotinha-voadora.png')
+    gameSound = loadSound('songs/trilha_jogo.mp3')
+    jumpSound = loadSound('songs/somPulo.mp3')
+  }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,3 @@
-function preload() {
-  backgroundImage = loadImage('images/background/floresta.png')
-  gameOverImage = loadImage('images/assets/game-over.png')
-  characterImage = loadImage('images/character/correndo.png')
-  enemyImage = loadImage('images/enemies/gotinha.png')
-  trollImage = loadImage('images/enemies/troll.png')
-  wingSlimeImage = loadImage('images/enemies/gotinha-voadora.png')
-  gameSound = loadSound('songs/trilha_jogo.mp3')
-  jumpSound = loadSound('songs/somPulo.mp3')
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background = new Background(backgroundImage, 3)
